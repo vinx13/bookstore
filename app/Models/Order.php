a95c58375407d8048281d0e330b042473ad3418e
Wuwei Lin <vincentl13x@gmail.com>
2017-03-16 23:50:07 +0800

    Use soft deletion

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,13 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
+    use SoftDeletes;
+
     public function user() {
         return $this->belongsTo('App\Models\User');
     }
     public function book() {
         return $this->belongsTo('App\Models\Book');
     }
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model
 {
+    use SoftDeletes;
+
     public function orders() {
         return $this->hasMany('App\Models\Order');
     }
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 }
