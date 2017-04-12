7a136cc8c27f4e58e282aa24fb81c1aa0fac4225
Wuwei Lin <vincentl13x@gmail.com>
2017-04-12 17:12:20 +0800

    Enable jackson hibernate5 module

--- src/main/java/me/vincentlin/bookstore/FastJacksonConfig.java
+++ src/main/java/me/vincentlin/bookstore/FastJacksonConfig.java
@@ -1,12 +1,20 @@
 package me.vincentlin.bookstore;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import static com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS;
 
 /**
  * Created by Vincent on 2017/4/6.
  */
+@Configuration
 public class FastJacksonConfig {
     @Bean
     public Module hibernate5Module() {
