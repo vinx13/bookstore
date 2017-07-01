6f479e7898e0ac9b1c523769bd0d47c7056cb520
Wuwei Lin <vincentl13x@gmail.com>
2017-07-01 13:16:23 +0800

    Add stored procedure

--- src/main/java/me/vincentlin/bookstore/dao/OrderRepository.java
+++ src/main/java/me/vincentlin/bookstore/dao/OrderRepository.java
@@ -1,13 +1,28 @@
 package me.vincentlin.bookstore.dao;
 
+import me.vincentlin.bookstore.model.Book;
 import me.vincentlin.bookstore.model.Order;
 import me.vincentlin.bookstore.model.projection.OrderExcerpt;
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.support.QueryDslJpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.NamedStoredProcedureQuery;
+import java.util.List;
 
 /**
  * Created by Vincent on 2017/3/25.
  */
 @RepositoryRestResource(excerptProjection = OrderExcerpt.class)
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>,JpaSpecificationExecutor {
+    @Query("select o from Order o join o.entries e where e.book.id = ?1")
+    List<Order> findByBook(Long bookId);
+
+    @Query("select o from Order o join o.entries e where e.book.genre.id = ?1")
+    List<Order> findByGenre(Long genreId);
+
+
 }
--- src/main/java/me/vincentlin/bookstore/dao/UserRepository.java
+++ src/main/java/me/vincentlin/bookstore/dao/UserRepository.java
@@ -1,9 +1,13 @@
 package me.vincentlin.bookstore.dao;
 
+import me.vincentlin.bookstore.model.Book;
 import me.vincentlin.bookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Vincent on 2017/3/25.
@@ -11,4 +15,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    List<User> findByUsernameStartsWith(@Param("name") String name);
 }
